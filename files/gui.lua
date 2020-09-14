2a88c61cacbb7b004238366e1e9be92bb0effa0c
The Horscht <realhorscht+github@gmail.com>
2020-09-14 22:39:30 +0200

    Add center point, increase size

--- files/gui.lua
+++ files/gui.lua
@@ -1,5 +1,3 @@
-local screen_width, screen_height = 427, 242
-
 gui = gui or GuiCreate()
 
 local id = 0
@@ -14,10 +12,8 @@ local open_or_close = HasFlagPersistent("locationtracker_hide_map") and "+" or "
 if GuiButton(gui, 2, 0, "["..open_or_close.."]", create_id()) then
   if HasFlagPersistent("locationtracker_hide_map") then
     RemoveFlagPersistent("locationtracker_hide_map")
-    GameSetPostFxParameter("uLocationTracker_sizes", screen_width, screen_height, 3, 3)
   else
     AddFlagPersistent("locationtracker_hide_map")
-    GameSetPostFxParameter("uLocationTracker_sizes", screen_width, screen_height, 0, 0)
   end
 end
 if not HasFlagPersistent("locationtracker_hide_map") then
--- files/initialize.lua
+++ files/initialize.lua
@@ -1,17 +1,14 @@
 local entity_id = GetUpdatedEntityID()
 local x, y = EntityGetTransform(entity_id)
-for y=0,9 do
-  for x=0,9 do
+for y=0,10 do
+  for x=0,10 do
     EntityAddComponent2(entity_id, "SpriteComponent", {
       image_file="mods/LocationTracker/_virtual/biome_map.xml",
       offset_x=-x,
       offset_y=-y,
-      -- has_special_scale=true,
-      -- special_scale_x=1.1,
-      -- special_scale_y=50,
       smooth_filtering=false,
-      -- z_index=-999,
       ui_is_parent=true,
+      alpha=0.8,
     })
   end
 end
